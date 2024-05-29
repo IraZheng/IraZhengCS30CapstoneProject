7d3f9599a6d16482157c668d2701483f340c4111
IraZheng <158590137+IraZheng@users.noreply.github.com>
2024-05-29 15:21:46 +0000

    basic shop and lootbox system

--- main.py
+++ main.py
@@ -10,11 +10,47 @@
 # Imports and Global Variables ------------------------------------------------
 import random
 
-cardList = ["common", "uncommon", "rare"]
-chanceList = [90, 9, 1]
+basicItemList = ["common", "uncommon", "rare", "epic", "legend"]
+basicChanceList = [59, 25, 10, 5, 1]
+rollAmount = 100
 # Functions -------------------------------------------------------------------
+def shopMenu():
+    while True:
+        print("Welcome to the shop")
+        print("What do you want to do?")
+        print("-buy\n-exit")
+        choice = input("-").lower()
+        if choice == "buy":
+            while True:
+                try:
+                    lootboxAmount = int(input("How many lootboxes do you buy: "))
+                except:
+                    print("please input a number")
+                else:
+                    obtainedItems = random.choices(basicItemList, basicChanceList, k=lootboxAmount)
+                    itemCounter = {"common": 0, "uncommon": 0, "rare": 0, 
+                                   "epic": 0, "legend": 0}
+                    for item in obtainedItems:
+                        if item in itemCounter:
+                            itemCounter[item] += 1
+                        else:
+                            itemCounter[item] = 1
+                    print(itemCounter)
+                    break
+        elif choice == "exit":
+            break
+        else:
+            print("Please choose one of the options listed above")
 
 
 # Main ------------------------------------------------------------------------
-print(random.choices(cardList, chanceList, k=100))
+'''obtainedItems = random.choices(basicItemList, basicChanceList, k=rollAmount)
+itemCounter = {}
+for item in obtainedItems:
+    if item in itemCounter:
+        itemCounter[item] += 1
+    else:
+        itemCounter[item] = 1
+print(itemCounter)'''
+shopMenu()
 
